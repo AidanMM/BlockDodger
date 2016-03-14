d9c5c965c270958c666375ff517767b469ae2096
Aidan McInerny <aidanmcinerny@Aidans-Mac-mini.local>
2016-03-14 03:46:45 -0400

    Changed most of the gameplay, added classes, game is in portrait mode now.

--- BlockDodger/GameScene.swift
+++ BlockDodger/GameScene.swift
@@ -7,230 +7,34 @@
 //
 
 import Foundation
-import AVFoundation
 import SpriteKit
 
-struct PhysicsCategory {
-    static let None      : UInt32 = 0
-    static let All       : UInt32 = UInt32.max
-    static let Block     : UInt32 = 0b1       // 1
-    static let Projectile: UInt32 = 0b10      // 2
-    static let Player    : UInt32 = 0b11      // 3
-}
-
-class GameScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContactDelegate {
+class GameScene: SKScene, UIGestureRecognizerDelegate {
     
-    let player = SKSpriteNode(imageNamed:"Box");
+    let player = Player(imageNamed:"Box");
     var total:Int = 0
     var scaleFactor = CGFloat(2.0)
     var interval = 2.0
     var blocksList: [SKSpriteNode] = []
     var gameOver = false //Determines if game is over or not
-    
-    var engine = AVAudioEngine();
-    var tone = AVTonePlayerUnit();
-    
+    let label = SKLabelNode(fontNamed: "Arial")
     
     override func didMoveToView(view: SKView) {
-        reScale(2.5);
-        player.position = CGPointMake(player.size.width / 2, (size.height / 2  % player.size.height) + player.size.height * CGFloat(total / 2) )
-        player.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: player.size.width / 2.0, height: player.size.width / 2.0))
-        player.physicsBody?.dynamic = true
-        player.physicsBody?.categoryBitMask = PhysicsCategory.Player
-        player.physicsBody?.contactTestBitMask = PhysicsCategory.Block
-        player.physicsBody?.collisionBitMask = PhysicsCategory.None
-        player.physicsBody?.usesPreciseCollisionDetection = true
-        
+        backgroundColor = UIColor(colorLiteralRed: 135/255, green: 206/255, blue: 250/255, alpha: 1.0)
+        player.initialize(self)
         addChild(player)
         
-        //Set up swiping gestures
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
-        swipeRight.direction = UISwipeGestureRecognizerDirection.Up
-        self.view!.addGestureRecognizer(swipeRight)
+        label.position = CGPointMake(size.width - size.height, 0)
+        label.fontSize = 20
+        addChild(label)
         
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: "respondToSwipeGesture:")
-        swipeDown.direction = UISwipeGestureRecognizerDirection.Down
-        self.view!.addGestureRecognizer(swipeDown)
-        
-        // set up tap gesture recognizer
-        let tap = UITapGestureRecognizer(target: self, action: "tapDetected:")
-        //        tap.numberOfTapsRequired = 2
-        //        tap.numberOfTouchesRequired = 2
-        tap.delegate = self
-        view.addGestureRecognizer(tap)
-        
-        physicsWorld.gravity = CGVectorMake(0, 0)
-        physicsWorld.contactDelegate = self
-        
-        runAction(SKAction.repeatAction(
-            SKAction.sequence([
-                SKAction.waitForDuration(interval),
-                SKAction.runBlock(generateBlockRow)
-                ]), count: 1
-            ))
-        
-        
-        let format = AVAudioFormat(standardFormatWithSampleRate: tone.sampleRate, channels: 1);
-        engine.attachNode(tone)
-        let mixer = engine.mainMixerNode;
-        engine.connect(tone, to: mixer, format: format)
-        do {
-            try engine.start()
-        } catch let error as NSError {
-            print(error)
-        }
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        
-    }
-    
-    func respondToSwipeGesture(gesture: UIGestureRecognizer) {
-        
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            var playerAction = SKAction.moveTo(CGPointMake(player.position.x, player.position.y), duration: 0.01)
-            
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizerDirection.Down:
-                if player.position.y - player.size.height > 0 {
-                    playerAction = SKAction.moveTo(CGPointMake(player.position.x, player.position.y - player.size.height), duration: 0.01)
-                }
-            case UISwipeGestureRecognizerDirection.Up:
-                if player.position.y + player.size.height < self.size.height {
-                    playerAction = SKAction.moveTo(CGPointMake(player.position.x, player.position.y + player.size.height), duration: 0.01)
-                }
-            default:
-                playerAction = SKAction.moveTo(CGPointMake(player.position.x, player.position.y), duration: 0.01)
-                break
-            }
-            
-            player.runAction(playerAction);
-        }
-    }
-    
-    func reScale(scale: CGFloat) {
-        scaleFactor = scale;
-        total = Int(self.size.height / scaleFactor / player.size.width)
-        player.setScale(scaleFactor);
-    }
-    
-    
-    func tapDetected(sender:UITapGestureRecognizer) {
-        // fire bullet
-        let projectile = SKSpriteNode(imageNamed: "Box");
-        projectile.setScale(scaleFactor);
-        projectile.color = SKColor.whiteColor()
-        projectile.position = CGPointMake(player.position.x, player.position.y /*+ player.size.height/2 + 5*/)
-        projectile.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: player.size.width / 2.0, height: player.size.width / 2.0))
-        projectile.physicsBody?.dynamic = true
-        projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
-        projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Block
-        projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
-        projectile.physicsBody?.usesPreciseCollisionDetection = true
-        
-        let moveAction = SKAction.moveByX(2000, y: 0, duration: 2)
-        let deleteAction = SKAction.removeFromParent()
-        let blockAction = SKAction.runBlock({print("Done moving, deleting")})
-        projectile.runAction(SKAction.sequence([moveAction,blockAction,deleteAction]))
-        addChild(projectile)
+        let colorize = SKAction.colorizeWithColor(UIColor(colorLiteralRed: 0, green: 25/255, blue: 50/255, alpha: 1.0), colorBlendFactor: 1, duration: 10)
+        runAction(colorize)
     }
     
     override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered
-        //TODO: Remove this code and resolve to a real end-state(probably no win condition)
-        //These are the temporary lose and win states
-        //When you get the correct ones just replace my random variable with the one you want them hooked up with
-        if win >= 100 && !gameOver {
-        gameOver = true
-        let gameOverScene = GameOverScene(size: size, won: false)
-        gameOverScene.scaleMode = scaleMode
-        let reveal = SKTransition.flipHorizontalWithDuration(0.5)
-        view?.presentScene(gameOverScene, transition: reveal)
-        }
-        win++*/
+        label.text = "\(player.level)"
         
-    }
-    
-    func generateBlockRow() {
-        let newScale = CGFloat(Float(arc4random()) / Float(UINT32_MAX)) * 3.0
-        reScale( newScale > 1.0 ? newScale : 1.0)
-        let emptySlot = Int(arc4random()) % total
-        
-        let peak = Int(arc4random()) % total
-        
-        for var i = 0; i < total; i++ {
-            if i != emptySlot {
-                let offSet = CGFloat(abs(peak - i)) * 2;
-                let double = Double(abs(peak - i));
-                playTone(double * 50 + 400);
-                let block = SKSpriteNode(imageNamed: "Box")
-                block.setScale(scaleFactor);
-                block.position = CGPointMake(size.width + block.size.width / 2 * offSet , (size.height / 2  % player.size.height) + player.size.height * CGFloat(i) )
-                
-                //Give the blocks physics bodies
-                //add collision
-                block.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: player.size.width / 2.0, height: player.size.width / 2.0)) // 1
-                block.physicsBody?.dynamic = true // 2
-                block.physicsBody?.categoryBitMask = PhysicsCategory.Block // 3
-                block.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
-                block.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
-                
-                // Create the actions
-                let actionMove = SKAction.moveTo(CGPointMake(-block.size.width / 2, block.position.y), duration: 5)
-                let actionMoveDone = SKAction.runBlock( {
-                    SKAction.removeFromParent()
-                    //player.setScale(newScale > 1.0 ? newScale : 1.0)
-                })
-                block.runAction(SKAction.sequence([actionMove, actionMoveDone]))
-                
-                addChild(block)
-            }
-        }
-        interval /= 1.2
-        if interval < 1.5 {
-            interval = 1.5
-        }
-        runAction(SKAction.repeatAction(
-            SKAction.sequence([
-                SKAction.waitForDuration(interval),
-                SKAction.runBlock(generateBlockRow)
-                ]), count: 1
-            ))
-    }
-    
-    func projectileDidCollideWithBlock(projectile:SKSpriteNode, block:SKSpriteNode) {
-        projectile.removeFromParent()
-        block.removeFromParent()
-    }
-    
-    func blockDidCollideWithPlayer(projectile:SKSpriteNode, block:SKSpriteNode) {
-        gameOver = true
-        let gameOverScene = GameOverScene(fileNamed: "GameOverScene")
-        gameOverScene!.scaleMode = scaleMode
-        let reveal = SKTransition.flipHorizontalWithDuration(0.5)
-        view?.presentScene(gameOverScene!, transition: reveal)
-        
-    }
-    
-    func didBeginContact(contact: SKPhysicsContact) {
-        var firstBody: SKPhysicsBody
-        var secondBody: SKPhysicsBody
-        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-            firstBody = contact.bodyA
-            secondBody = contact.bodyB
-        } else {
-            firstBody = contact.bodyB
-            secondBody = contact.bodyA
-        }
-        
-        if ((firstBody.categoryBitMask == PhysicsCategory.Block) &&
-            (secondBody.categoryBitMask == PhysicsCategory.Projectile)) {
-                projectileDidCollideWithBlock(firstBody.node as! SKSpriteNode, block: secondBody.node as! SKSpriteNode)
-        }
-        else if ((firstBody.categoryBitMask == PhysicsCategory.Block ) &&
-            (secondBody.categoryBitMask == PhysicsCategory.Player )) {
-                blockDidCollideWithPlayer(firstBody.node as! SKSpriteNode, block: secondBody.node as! SKSpriteNode)
-        }
     }
     
     func gestureRecognizer(gestureRecognizer: UIGestureRecognizer,
@@ -238,70 +42,8 @@ class GameScene: SKScene, UIGestureRecognizerDelegate, SKPhysicsContactDelegate 
             return true
     }
     
-    func playTone(freq: Double) {
-        engine.mainMixerNode.volume = 1.0
-        tone.frequency = freq;
-        //tone.amplitude = 1.0;
-        tone.preparePlaying()
-        tone.play()
-        print(tone.playing)
-    }
-}
-
-//I did not create this!
-//https://github.com/ooper-shlab/ToneGenerator1.0-Swift/blob/master/ToneGenerator/ViewController.swift
-class AVTonePlayerUnit: AVAudioPlayerNode {
-    let bufferCapacity: AVAudioFrameCount = 512
-    let sampleRate: Double = 44_100.0
+    /*func didBeginContact(contact: SKPhysicsContact) {
+        player.didBeginContact(contact);
+    }*/
     
-    var frequency: Double = 440.0
-    var amplitude: Double = 0.25
-    
-    private var theta: Double = 0.0
-    private var audioFormat: AVAudioFormat!
-    
-    override init() {
-        super.init()
-        audioFormat = AVAudioFormat(standardFormatWithSampleRate: sampleRate, channels: 1)
-    }
-    
-    func prepareBuffer() -> AVAudioPCMBuffer {
-        let buffer = AVAudioPCMBuffer(PCMFormat: audioFormat, frameCapacity: bufferCapacity)
-        fillBuffer(buffer)
-        return buffer
-    }
-    
-    func fillBuffer(buffer: AVAudioPCMBuffer) {
-        let data = buffer.floatChannelData[0]
-        let numberFrames = buffer.frameCapacity
-        var theta = self.theta
-        let theta_increment = 2.0 * M_PI * self.frequency / self.sampleRate
-        
-        for frame in 0..<Int(numberFrames) {
-            data[frame] = Float32(sin(theta) * amplitude)
-            
-            theta += theta_increment
-            if theta > 2.0 * M_PI {
-                theta -= 2.0 * M_PI
-            }
-        }
-        buffer.frameLength = numberFrames
-        self.theta = theta
-    }
-    
-    func scheduleBuffer() {
-        let buffer = prepareBuffer()
-        self.scheduleBuffer(buffer) {
-            if self.playing {
-                self.scheduleBuffer()
-            }
-        }
-    }
-    
-    func preparePlaying() {
-        scheduleBuffer()
-        scheduleBuffer()
-        scheduleBuffer()
-        scheduleBuffer()
-    }
 }
--- BlockDodger/Player.swift
+++ BlockDodger/Player.swift
@@ -9,16 +9,219 @@
 import Foundation
 import SpriteKit
 
-/*class Player : SKSpriteNode {
-    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+struct PhysicsCategory {
+    static let None      : UInt32 = 0
+    static let All       : UInt32 = UInt32.max
+    static let Block     : UInt32 = 0b1       // 1
+    static let Projectile: UInt32 = 0b10      // 2
+    static let Player    : UInt32 = 0b11      // 3
+    static let AmmoBlock : UInt32 = 0b100     // 4
+    static let sizeBlock : UInt32 = 0b101     // 5
+    static let scaleBlock: UInt32 = 0b110     // 6
+}
+
+class Player : SKSpriteNode, SKPhysicsContactDelegate {
+    
+    var myScene:SKScene?
+    var total:Int = 0
+    var previousPanX:CGFloat = 0.0
+    var level:CGFloat = 0
+    var emptySlot:Int = 0
+    var playerScale:CGFloat = 0.5
+    var blockScale:CGFloat = 2.0
+    var ammo:Int = 3
+    
+    func initialize(scene:GameScene) {
+        myScene = scene
+        //Set up initial location
+        total = Int(scene.size.height / self.size.width)
+        //self.position = CGPointMake(self.size.width / 2, (size.height / 2  % self.size.height) + self.size.height * CGFloat(total / 2) )
+        self.setScale(playerScale)
+        self.position = CGPointMake(scene.size.width / 2, scene.size.height - self.size.height / 2)
+        self.colorBlendFactor = 1.0
+        self.color = SKColor.redColor()
+        
+        let moveAction = SKAction.moveByX(0, y: -200, duration: 2.0)
+        runAction(moveAction)
+        
+        
+        //set up physics body
+        self.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.size.width / 2.0, height: self.size.width / 2.0))
+        self.physicsBody?.dynamic = true
+        self.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        self.physicsBody?.contactTestBitMask = PhysicsCategory.Block
+        self.physicsBody?.collisionBitMask = PhysicsCategory.None
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        
+        scene.physicsWorld.contactDelegate = self
+        scene.physicsWorld.gravity = CGVectorMake(0.0, 0.0);
+        
+        //set up gestures
+        let pan = UIPanGestureRecognizer(target: self, action: "panDetected:")
+        //pan.minimumNumberOfTouches = 2
+        pan.delegate = scene
+        scene.view!.addGestureRecognizer(pan)
+        
+        // set up tap gesture recognizer
+        let tap = UITapGestureRecognizer(target: self, action: "tapDetected:")
+        //tap.numberOfTapsRequired = 1
+        //tap.numberOfTouchesRequired = 2
+        tap.delegate = scene
+        scene.view!.addGestureRecognizer(tap)
+        
+        emptySlot = Int(arc4random()) % (total - 2)
+        
+        if emptySlot <= 3 {
+            emptySlot = 4
+        }
+        print(emptySlot)
+        generateBlockRow()
+    }
+    
+    //MARK: - Gameplay Functions
+    
+    func shoot() {
+        if ammo > 0 {
+            let projectile = SKSpriteNode(imageNamed: "Box")
+            projectile.colorBlendFactor = 1.0
+        	projectile.color = SKColor.blackColor()
+            projectile.position = CGPointMake(self.position.x, self.position.y)
+            projectile.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: self.size.width / 2.0, height: self.size.width / 2.0))
+            projectile.physicsBody?.dynamic = true
+            projectile.physicsBody?.categoryBitMask = PhysicsCategory.Projectile
+            projectile.physicsBody?.contactTestBitMask = PhysicsCategory.Block
+            projectile.physicsBody?.collisionBitMask = PhysicsCategory.None
+            projectile.physicsBody?.usesPreciseCollisionDetection = true
+            projectile.setScale(playerScale)
+            
+            let moveAction = SKAction.moveByX(0, y: -myScene!.size.height, duration: 2.0)
+            let deleteAction = SKAction.removeFromParent()
+            let blockAction = SKAction.runBlock({print("Done moving, deleting")})
+            projectile.runAction(SKAction.sequence([moveAction,blockAction,deleteAction]))
+            myScene!.addChild(projectile)
+            ammo--;
+        }
+    }
+    
+    func update(dt:Float) {
         
     }
     
-    convenience init() {
+    func generateBlockRow() {
+        let slotOffset = drand48() > 0.5 ? -1 : 1
+        if emptySlot - slotOffset < 3 {
+            emptySlot = 3
+        } else if emptySlot + slotOffset >= total {
+            emptySlot = total - 2
+        } else if emptySlot < 5 {
+            emptySlot = 5
+        } else {
+            emptySlot += slotOffset
+        }
+        print(emptySlot)
+        let colorize = SKAction.colorizeWithColor(SKColor(colorLiteralRed: 87/255, green: 59/255, blue: 12/255, alpha: 1.0), colorBlendFactor: 1, duration: (5.0 - Double(level / 500.0)))
+        for var i = 0; i <= total; i++ {
+            if i != emptySlot && i != emptySlot + 1 && i != emptySlot - 1 {
+                let block = SKSpriteNode(imageNamed: "Box")
+                block.position = CGPointMake(block.size.width * CGFloat(i) + block.size.width / 2, 0 - block.size.height)
+                
+                block.color = SKColor(colorLiteralRed: 57/255, green: 39/255, blue: 0/255, alpha: 1.0)
+                block.colorBlendFactor = 1.0
+                
+                block.runAction(colorize)
+                
+                //Give the blocks physics bodies
+                //add collision
+                block.physicsBody = SKPhysicsBody(rectangleOfSize: CGSize(width: block.size.width, height: block.size.width)) // 1
+                block.physicsBody?.dynamic = true // 2
+                block.physicsBody?.categoryBitMask = PhysicsCategory.Block // 3
+                block.physicsBody?.contactTestBitMask = PhysicsCategory.Projectile // 4
+                block.physicsBody?.collisionBitMask = PhysicsCategory.None // 5
+                
+                // Create the actions
+                let actionMove = SKAction.moveTo(CGPointMake(block.position.x, myScene!.size.height + block.size.height), duration: (5.0 - Double(level / 500.0)) )
+                let actionMoveDone = SKAction.runBlock( {
+                    SKAction.removeFromParent()
+                })
+                block.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+                
+                myScene!.self.addChild(block)
+            }
+        }
+        level++;
+        runAction(SKAction.repeatAction(
+            SKAction.sequence([
+                SKAction.waitForDuration(0.2),
+                SKAction.runBlock(generateBlockRow)
+                ]), count: 1
+            ))
+    }
+    
+    //MARK: - Gestures -
+    func panDetected(sender:UIPanGestureRecognizer) {
+        if myScene!.view != nil {
+        // retrieve pan movement along the x-axis of the view since the gesture began
+        let currentPanX = sender.translationInView(myScene!.view!).x
+        
+        // calculate deltaX since last measurement
+        let deltaX = currentPanX - previousPanX
+        
+        var newLoc = CGPointMake(self.position.x + deltaX * 2.0 , self.position.y)
+        if newLoc.x > myScene?.size.height {
+            newLoc.x = (myScene?.size.height)!
+        } else if newLoc.x < myScene!.size.width - (myScene?.size.height)! {
+            newLoc.x = myScene!.size.width - (myScene?.size.height)!
+        }
+        self.position = newLoc
+        
+        // if the gesture has completed
+        if sender.state == .Ended {
+            previousPanX = 0
+        } else {
+            previousPanX = currentPanX
+        }
+        }
+    }
+    
+    func tapDetected(sender:UITapGestureRecognizer) {
+        // fire bullet
+        shoot();
+    }
+    
+    //MARK: - Collision -
+    func projectileDidCollideWithBlock(projectile:SKSpriteNode, block:SKSpriteNode) {
+        projectile.removeFromParent()
+        //block.removeFromParent()
+    }
+    
+    func blockDidCollideWithPlayer(projectile:SKSpriteNode, block:SKSpriteNode) {
+        let gameOverScene = GameOverScene(fileNamed: "GameOverScene")
+        gameOverScene!.scaleMode = (myScene?.scaleMode)!
+        let reveal = SKTransition.flipHorizontalWithDuration(0.5)
+        myScene!.view!.presentScene(gameOverScene!, transition: reveal)
+        removeFromParent()
         
     }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    
+    func didBeginContact(contact: SKPhysicsContact) {
+        var firstBody: SKPhysicsBody
+        var secondBody: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            firstBody = contact.bodyA
+            secondBody = contact.bodyB
+        } else {
+            firstBody = contact.bodyB
+            secondBody = contact.bodyA
+        }
+        
+        if ((firstBody.categoryBitMask == PhysicsCategory.Block) &&
+            (secondBody.categoryBitMask == PhysicsCategory.Projectile)) {
+                projectileDidCollideWithBlock(firstBody.node as! SKSpriteNode, block: secondBody.node as! SKSpriteNode)
+        }
+        else if ((firstBody.categoryBitMask == PhysicsCategory.Block ) &&
+            (secondBody.categoryBitMask == PhysicsCategory.Player )) {
+                blockDidCollideWithPlayer(firstBody.node as! SKSpriteNode, block: secondBody.node as! SKSpriteNode)
+        }
     }
-}*/
+
+}
